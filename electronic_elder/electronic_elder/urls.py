df0f2eb17b82c555ae1c346628c95d1de06f19ee
Tranel <iebk17@gmail.com>
2023-08-22 18:22:20 +1000

    Move front in django project

--- electronic_elder/electronic_elder/urls.py
+++ electronic_elder/electronic_elder/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products.views import index,contacts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('contacts/', contacts, name='contacts'),
 ]
