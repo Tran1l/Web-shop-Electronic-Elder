908c068acdae062ac8e4c3bba060244d26c8e998
Tranel <iebk17@gmail.com>
2023-08-30 22:06:48 +1000

    make models to catalog

--- electronic_elder/products/admin.py
+++ electronic_elder/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from products.models import ProductCatagory, Product
+
+admin.site.register(Product)
+admin.site.register(ProductCatagory)
--- electronic_elder/products/models.py
+++ electronic_elder/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class ProductCatagory(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=1)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='media')
+    category = models.ForeignKey(to=ProductCatagory, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return f'Продукт: {self.name} | Категория: {self.category.name}'
+    
+
