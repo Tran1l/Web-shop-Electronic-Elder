9186e50420568d5c24ca53cc701f35897669d9a3
Tranel <iebk17@gmail.com>
2023-09-03 23:05:56 +1000

    fixed path to gunicorn

--- electronic_elder/entrypoint.sh
+++ electronic_elder/entrypoint.sh
@@ -2,7 +2,6 @@
 python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
-# python manage.py migrate
 python manage.py collectstatic --no-input
 gunicorn --bind 0.0.0.0:8000 electronic_elder.wsgi:application
 exec "$@"
--- electronic_elder/gunicorn_config.py
+++ electronic_elder/gunicorn_config.py
@@ -3,11 +3,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 BASE_DIR_PARENT = Path(__file__).resolve().parent.parent
 
-command = f'{BASE_DIR}/venv/bin/gunicorn'
-pythonpath = f'{BASE_DIR}'
-bind = '127.0.0.1:8000'
+command = f"{BASE_DIR}/env/bin/gunicorn"
+pythonpath = f"{BASE_DIR}"
+bind = "127.0.0.1:8000"
 workers = 5
-user = 'tran1l'
+user = "tran1l"
 limit_request_fields = 32000
 limit_request_field_size = 0
-raw_env = 'DJANGO_SETTINGS_MODULE=electronic_elder.settings'
+raw_env = "DJANGO_SETTINGS_MODULE=electronic_elder.settings"
